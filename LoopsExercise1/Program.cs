f3db74fe4ed8b3f6c85b57359e1c53af9ed6a49d
Keanon Born <disposingmoon1@gmail.com>
2024-01-24 18:45:14 -0500

    completed loop exercise

---
+++ LoopsExercise1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LoopsExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void IsEqual(int number1, int number2)
+            {
+                if (number1 == number2)
+                {
+
+                }
+            }
+            static void PositiveOrNegative(int number)
+            {
+                if (number >= 0)
+                {
+                    Console.WriteLine($"{number} is positive");
+                }
+                else (number < 0)
+                {
+                    Console.WriteLine($"{number} is negative.");
+                }
+
+            }
+            static void CanPersonVote()
+            {
+                int age = int.Parse(Console.ReadLine());
+                if (age >= 18)
+                {
+                    Console.WriteLine($"You can vote!");
+                }
+                else (age < 18)
+                {
+                    Console.WriteLine($"Sorry, you cannot vote.");
+                }
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
